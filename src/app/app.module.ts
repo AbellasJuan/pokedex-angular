a40f53b283e4a3d57f8d0fe8443e21a88543db2a
abellasjuan <juanvictoraa@gmail.com>
2022-07-25 10:20:23 -0300

    style: create 'pokemon-list' style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
+import { PokemonService } from '../_services/pokemon.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PokemonListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, PokemonListComponent, PokemonCardComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [PokemonService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
